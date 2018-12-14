78a57191beb58bfb4a5a70a9644f76f7fbb2e30d
Emil Niyaz <niyaz.emil@gmail.com>
2018-12-13 21:56:06 -0500

    Four types of function

---
+++ essentials/functions.py
@@ -0,0 +1,31 @@
+# Default arguments
+#
+# def defaultArg(name, msg = "hello!"):
+#     print(name, msg)
+#
+# defaultArg('name')
+
+# Required arguments
+#
+# def requiredArg(str, num):
+#     print(str, num)
+# requiredArg('Hi', 1)
+#
+# Keyword arguments
+
+# def keyword_arg(name, role):
+#     print()
+#
+# keyword_arg(name='Tom', role='manager')
+# or
+# keyword_arg(role='manager', name='Tom')
+
+# The variable of arguments
+# def var_length_args(*varargs):
+#     for val in varargs:
+#         print(val)
+#     return;
+# var_length_args(55)
+# var_length_args(55,50,30,20)
+
+
