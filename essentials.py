e5de914b15ff9c4b1bafdd101ab4d0fe42805b36
nuulu143 <Niiaz_uulu@comcast.com>
2018-12-16 23:04:22 -0500

    Added some essential changes

---
+++ essentials.py
@@ -0,0 +1,56 @@
+# For loop
+# numbers = [1,2,3,4,5]
+#
+# for num in numbers:
+#     print(num)
+#
+# words = ['bus', 'car', 'train']
+#
+# if 'bus' in words:
+#     print('Yeap')
+#
+
+# Turples
+
+# t = 'a', 'b', 'c'
+# t = ('a', 'b', 'c')
+
+# t = ('a') <type> str
+# t = ('a',) <type> turple
+
+# Insert an item to turple
+# sometuple + (someitem,)
+
+# print(t[1: len(t) - 1])
+
+# Dictionaries
+
+# eng2sp = {}
+# >>> class 'dict'
+
+# dictionary = {key: value}  | associative arrays | objects | symbol tables | map |
+
+# del dictionary[key]
+# len(dictionary)
+
+# key in dictionary TRUE or FALSE
+#             ^
+#        Better than
+#             |
+# dictionary[key]   value or ERROR
+
+# sorted(dictionary)  sorted keys as a list
+
+
+# While loop
+
+# while condition:
+#     body
+
+# a = range(5)      range(0,5)
+# >>> class 'range'
+
+# for index in range(len(list)):
+#     print index
+# print(type(a))
+
--- essentials/for-loop.py
+++
@@ -1,8 +0,0 @@
-numbers = [1,2,3,4,5]
-for num in numbers:
-    print(num)
-
-words = ['bus', 'car', 'train']
-
-if 'bus' in words:
-    print('Yeap')
